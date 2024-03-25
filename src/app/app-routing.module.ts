12135e8f7da0764ba91b678022638f75078ae713
Azizul-arif <azizularif94@gmail.com>
2024-03-26 02:07:29 +0600

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { SuccessComponent } from './success/success.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'personal-details',component:PersonalDetailsComponent},
+  {path:'contact-details',component:ContactDetailsComponent},
+  {path:'skills',component:SkillsComponent},
+  {path:'work-experience',component:WorkExperienceComponent},
+  {path:'success',component:SuccessComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { HomeComponent } from './home/home.component';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SuccessComponent } from './success/success.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalDetailsComponent,
+    ContactDetailsComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    HomeComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterLink,
+    FormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
